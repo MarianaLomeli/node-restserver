d66ba8ec40685417f11d318dce04c7a8a544dc2f
Salvador Hernandez <chavahdez92@gmail.com>
2019-07-26 10:19:11 -0500

    Rest Server-Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,13 @@
 process.env.PORT = process.env.PORT || 3000;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://user1:05Ua0AlY6FwNLXcw@cluster0-z6rza.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
